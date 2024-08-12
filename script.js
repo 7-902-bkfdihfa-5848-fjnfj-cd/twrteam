938408e38e85c3b284b58a79998ec97535feb73b
7-902-bkfdihfa-5848-fjnfj-cd <alexandru.barza603@gmail.com>
2024-08-12 19:55:21 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-ScrollReveal({
-    reset: false,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-})
-
-ScrollReveal().reveal('#hero h1, .about .content p, #hero h2,  #about .content h3, .section-title h2, #hero a, a', { origin: 'top' });
-ScrollReveal().reveal('.about .icon-boxes .icon-box,  .services .icon-box', { origin: 'bottom' }); 
