1d18a27a7f40106dc02701cf1d8dcd75de9ea31f
7-902-bkfdihfa-5848-fjnfj-cd <alexandru.barza603@gmail.com>
2024-08-12 19:56:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: false,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('#hero h1, .about .content p, #hero h2,  #about .content h3, .section-title h2, #hero a', { origin: 'top' });
+ScrollReveal().reveal('.about .icon-boxes .icon-box,  .services .icon-box, .social-media a', { origin: 'bottom' }); 
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+})
